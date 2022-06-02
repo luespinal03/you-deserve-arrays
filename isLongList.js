e80717c702e9dd937aaa95b5bbb4695babce8fda
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-06-02 14:12:09 -0700

    isLongList

--- isLongList.js
+++ isLongList.js
@@ -2,14 +2,13 @@ const prompt = require('prompt-sync')({
     sigint: true
 });
 
-let userArray = (prompt("Enter an array: "))
+let userArray = JSON.parse(prompt("Enter an array: "))
 console.log(userArray)
 
-while (userArray.length < 10 === true) {
+if (userArray.length < 10) {
     console.log("Your array must be at least 10 characters long, try again")
-        (prompt("Enter an array: "))
-    console.log(userArray)
+    
 }
-if (userArray.length > 10 === true) {
+else if (userArray.length >= 10 ) {
     console.log("Your array is at least 10 characters long, good job")
 }
